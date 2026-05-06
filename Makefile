549bf2a5ffb47d324d62ac0506cf4931670e757f
nicofgr <107516938+nicofgr@users.noreply.github.com>
2026-05-06 20:05:04 -0300

    First commit

---
+++ Makefile
@@ -0,0 +1,14 @@
+build: 
+	#clang -I./include/ -std=c99 -Wall ./src/*.c -lSDL2 -lGL -lX11 -lpthread -lXrandr -lXi -ldl -lm -o  saida.out
+	#clang -I./include/ -std=c99 -Wall -Werror -fsanitize=address ./src/*.c -lSDL2 -lGL -lX11 -lpthread -lXrandr -lXi -ldl -lm -o  saida.out
+	clang -I./include/ -std=c99 -Wall -fsanitize=address ./src/*.c -lSDL2 -lGL -lX11 -lpthread -lXrandr -lXi -ldl -lm -o  saida.out
+	#Turn -fsanitize off for release build
+
+profile:
+	clang -I./include/ -std=c99 -Wall -fsanitize=address ./src/*.c -lSDL2 -lGL -lX11 -lpthread -lXrandr -lXi -ldl -lm -pg -o saida.out
+
+run:
+	./saida.out
+
+clean:
+	rm ./saida.out
---
+++ src/main.c
@@ -0,0 +1,397 @@
+//
+//#include "cglm/affine-pre.h"
+//#include "cglm/affine-pre.h"
+#include "cglm/affine.h"
+#include "cglm/mat4.h"
+#include "cglm/types.h"
+#include "cglm/util.h"
+#include "cglm/vec3.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_keyboard.h>
+#include <SDL2/SDL_keycode.h>
+#include <SDL2/SDL_log.h>
+#include <SDL2/SDL_mouse.h>
+#include <SDL2/SDL_scancode.h>
+#include <SDL2/SDL_timer.h>
+#include <SDL2/SDL_video.h>
+#include <glad/glad.h>
+#include <stdatomic.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h> // for wait time
+#include <cglm/cglm.h>
+
+// My defines
+#define SCREEN_WIDTH   600
+#define SCREEN_HEIGHT  600
+#define TRUE  1
+#define FALSE 0
+#define TARGET_FPS 90
+#define FRAME_TARGET_TIME 1000/TARGET_FPS
+#define FOV 70
+
+int rotate = FALSE;
+float rotate_speed = -1.0f/0.9; // frequency
+vec3 translation = {0.0f, 0.0f, 0.0f};
+
+SDL_Window*   glWindow = NULL;
+SDL_GLContext glContext = NULL;
+
+float yaw = -90.0f;
+float pitch = 0.0f;
+
+const char *vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"uniform mat4 model;\n"
+"uniform mat4 view;\n"
+"uniform mat4 projection;\n"
+"uniform float sizeMultiplier;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = projection*view*model*vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"   gl_PointSize = min((50*sizeMultiplier)/gl_Position.z, 400.0f);\n"
+"}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"uniform vec4 ourColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = ourColor;\n"
+"}\0";
+
+unsigned int vertexShader;
+unsigned int fragmentShader;
+unsigned int shaderProgram;
+unsigned int VAO;
+unsigned int VBO;
+
+unsigned int VAO_faces;
+unsigned int VBO_faces;
+unsigned int EBO_faces;
+
+char* opened_file = NULL;
+int last_frame_time = 0;
+int lastTime = 0;
+struct nk_context *ctx;
+
+
+void init(void);
+void reshape(void);
+void display(void);
+
+void swapFloat(float* x1 , float* x2){
+        float aux = *x1;
+        *x1 = *x2;
+        *x2 = aux;
+}
+
+typedef struct Color_RGBA{
+        float R;
+        float G;
+        float B;
+        float A;
+}Color_RGBA;
+
+vec3 camera_pos   = {0.0f, 0.0f,  7.0f};
+vec3 camera_front = {0.0f, 0.0f, -1.0f};
+vec3 camera_up    = {0.0f, 1.0f,  0.0f};
+Color_RGBA red_color = {0.85f, 0.02f, 0.12f, 0.5f};
+Color_RGBA green_color = {0.20f, 1.0f, 0.69f, 0.5f};
+
+float verts[] = {0.5f, 0.5f, 0.0f,
+                 0.0f, 0.0f, 0.0f,
+                 1.0f, 0.0f, 0.0f};
+
+void draw_object(const Color_RGBA color){
+        //float* verts = HE_get_object_verts_as_array(current_object);
+        int size = 3;
+
+        mat4 model;
+        glm_mat4_identity(model);
+        //glm_scale(model, (vec4){0.1f, 0.1f, 0.1f, 1.0f});
+        glm_translate(model, translation);
+        if(rotate == TRUE)
+                glm_rotate(model, rotate_speed*((float)SDL_GetTicks()/1000.0f)*GLM_PI*2, (vec3){0.0f, 1.0f, 0.0f});
+
+        mat4 view;
+        glm_mat4_identity(view);
+        glm_translate(view, translation);
+
+        vec3 target_dir;
+
+        vec3 direction;
+        direction[0] = cos(glm_rad(yaw)) * cos(glm_rad(pitch));
+        direction[1] = sin(glm_rad(pitch));
+        direction[2] = sin(glm_rad(yaw)) * cos(glm_rad(pitch));
+        glm_normalize_to(direction, camera_front);
+        glm_vec3_add(camera_pos, camera_front, target_dir);
+        glm_lookat(camera_pos, target_dir, camera_up, view);
+
+        mat4 proj;
+        glm_mat4_identity(proj);
+        glm_perspective(glm_rad(FOV), (float)SCREEN_WIDTH/(float)SCREEN_HEIGHT, 0.1f, 100.0f, proj);
+
+        printf("HELLO \n");
+        fflush(stdout);
+
+        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+        int transformLocation   = glGetUniformLocation(shaderProgram, "model");
+        int viewLocation        = glGetUniformLocation(shaderProgram, "view");
+        int projLocation        = glGetUniformLocation(shaderProgram, "projection");
+        int sizeMultiplier      = glGetUniformLocation(shaderProgram, "sizeMultiplier");
+        glUseProgram(shaderProgram);
+        glUniform4f(vertexColorLocation, color.R, color.G, color.B, color.A);
+        glUniformMatrix4fv(transformLocation, 1, GL_FALSE, (const float*)model);
+        glUniformMatrix4fv(viewLocation, 1, GL_FALSE, (const float*)view);
+        glUniformMatrix4fv(projLocation, 1, GL_FALSE, (const float*)proj);
+        glUniform1f(sizeMultiplier, 1);
+
+        glBindVertexArray(VAO_faces);
+
+        glBindBuffer(GL_ARRAY_BUFFER, VBO_faces);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
+
+        //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO_faces);
+        //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int)*size, verts, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glDrawElements(GL_TRIANGLES, size , GL_UNSIGNED_INT, 0);
+        //free(verts);
+}
+
+
+void init() {
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glEnable(GL_PROGRAM_POINT_SIZE);
+        glEnable(GL_MULTISAMPLE);
+        glEnable(GL_DEPTH_TEST);
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+        int success;
+        char infoLog[512];
+        vertexShader = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+        glCompileShader(vertexShader);
+        glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+        if(!success){
+                glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+                printf("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n %s\n", infoLog);
+                printf("%d\n", glGetError());
+                exit(1);
+        }
+
+        fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+        glCompileShader(fragmentShader);
+        glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+        if(!success){
+                glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+                printf("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n %s\n", infoLog);
+                exit(1);
+        }
+
+        shaderProgram = glCreateProgram();
+        glAttachShader(shaderProgram, vertexShader);
+        glAttachShader(shaderProgram, fragmentShader);
+        glLinkProgram(shaderProgram);
+        glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+        if(!success) {
+                glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+                printf("ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n %s\n", infoLog);
+                exit(1);
+        }
+        glDeleteShader(vertexShader);
+        glDeleteShader(fragmentShader);
+
+
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+
+        glGenVertexArrays(1, &VAO_faces);
+        glGenBuffers(1, &VBO_faces);
+        glGenBuffers(1, &EBO_faces);
+
+
+        glBindVertexArray(VAO);
+        /**
+          glBindBuffer(GL_ARRAY_BUFFER, VBO);
+          glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+         **/
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+
+}
+int update_item = FALSE;
+
+void input(int * quit){
+        SDL_Event e;
+        const float camera_speed = 0.1f;
+        const Uint8* states = SDL_GetKeyboardState(NULL);
+        while(SDL_PollEvent(&e)){
+                switch(e.type){
+                        case SDL_QUIT:
+                                *quit = TRUE;
+                                break;
+                        case SDL_KEYDOWN:
+                                if(e.key.keysym.sym == SDLK_ESCAPE)
+                                        *quit = TRUE;
+                                if(e.key.keysym.sym == SDLK_TAB){
+                                        if(SDL_GetRelativeMouseMode() == SDL_TRUE){
+                                                SDL_SetRelativeMouseMode(SDL_FALSE);
+                                        }else{
+                                                SDL_SetRelativeMouseMode(SDL_TRUE);
+                                                SDL_GetRelativeMouseState(NULL, NULL);
+                                        }
+                                }
+                                break;  
+                        case SDL_DROPFILE:
+                                if(opened_file != NULL)
+                                        free(opened_file);
+                                opened_file = (char*)malloc(sizeof(char)*(strlen(e.drop.file)+1));
+                                strcpy(opened_file, e.drop.file);
+                                printf("File dropped: %s\n", opened_file);
+                                update_item = TRUE;
+                                SDL_free(e.drop.file);
+                                break;
+                }
+        }
+        if(states[SDL_SCANCODE_W]){
+                vec3 test;
+                glm_vec3_copy(camera_front, test);
+                test[1] = 0.0f;
+                glm_vec3_muladds(test, camera_speed, camera_pos); //pos += (front*spd)
+        }
+        if(states[SDL_SCANCODE_S]){
+                vec3 test;
+                glm_vec3_copy(camera_front, test);
+                test[1] = 0.0f;
+                glm_vec3_muladds(test, -camera_speed, camera_pos);
+        }
+        if(states[SDL_SCANCODE_A]){
+                vec3 aux;
+                glm_vec3_crossn(camera_front, camera_up, aux);
+                glm_vec3_muladds(aux, -camera_speed, camera_pos);
+        }
+        if(states[SDL_SCANCODE_D]){
+                vec3 aux;
+                glm_vec3_crossn(camera_front, camera_up, aux);
+                glm_vec3_muladds(aux, camera_speed, camera_pos);
+        }
+
+        const float sensitivity = 0.25;
+        int x = 0, y = 0;
+        if(SDL_GetRelativeMouseMode() == SDL_TRUE)
+                SDL_GetRelativeMouseState(&x, &y);
+        yaw += x*sensitivity;
+        pitch -= y*sensitivity;
+        if(pitch > 89.9f)
+                pitch = 89.9f;
+        if(pitch < -89.9f)
+                pitch = -89.9f;
+        if(0){
+        printf("%d, %d\n", x, y);
+        printf("Pos: %.2f, %.2f, %.2f\n", camera_pos[0], camera_pos[1], camera_pos[2]);
+        printf("Fnt: %.2f, %.2f, %.2f\n", camera_front[0], camera_front[1], camera_front[2]);
+        printf("Up:  %.2f, %.2f, %.2f\n\n", camera_up[0], camera_up[1], camera_up[2]);
+        }
+}
+
+void update(int* step){
+
+        int wait_time = FRAME_TARGET_TIME - (SDL_GetTicks() - last_frame_time);
+        if(wait_time > 0 && wait_time <= FRAME_TARGET_TIME)
+                SDL_Delay(wait_time);
+
+        float delta_time = (SDL_GetTicks() - last_frame_time) / 1000.0f;
+        last_frame_time = SDL_GetTicks();
+
+        *step = 0;
+        float currentTime = SDL_GetTicks();
+        if(currentTime - lastTime >= 500.0f){ // Updates every x seconds
+                *step = 1;
+                lastTime = SDL_GetTicks();
+        }
+}
+
+typedef struct{
+        char* string;
+        unsigned int size;
+} String;
+
+int option_selected = 0;
+int step_draw = TRUE;
+int selected_vertex = 0;
+int selected_face = 0;
+int selected_edge = 0;
+
+void draw(const int step){
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glEnable(GL_DEPTH_TEST);
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+        draw_object(red_color);
+}
+
+int main(int argc, char** argv) {
+        if(SDL_Init(SDL_INIT_EVERYTHING) != 0){
+                printf("SDL2 could not initialize video subsystem\n");
+                exit(1);
+        }
+
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+        SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+        SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+        SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
+        SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4);
+        glWindow = SDL_CreateWindow("OpenGL 3.3", 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_OPENGL);
+        if(glWindow == NULL){
+                printf("Error creating window\n");
+                exit(1);
+        }
+
+        glContext = SDL_GL_CreateContext(glWindow);
+        if(glContext == NULL){
+                printf("Error creating GL context\n");
+                exit(1);
+        }
+
+        if(!gladLoadGLLoader(SDL_GL_GetProcAddress)){
+                puts("glad was not initialized");
+                exit(1);
+        }
+        glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+
+        int quit = FALSE;
+        int counter = 0;
+
+        init();
+        
+        while(quit == FALSE){
+                input(&quit);
+                update(&counter);
+
+                draw(counter);
+                SDL_GL_SwapWindow(glWindow);
+                //quit = TRUE;
+
+        }
+        SDL_GL_DeleteContext(glContext);
+        SDL_DestroyWindow(glWindow);
+        SDL_Quit();
+        free(opened_file);
+        return 0;
+}
